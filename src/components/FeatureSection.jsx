57e4101a6119d9731dea2f823e4010cb2272f8d1
dhruvv99 <dhruvakanive4468@gmail.com>
2024-05-29 20:36:08 +0530

    work

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -12,7 +12,7 @@ px-2 py-1 uppercase '>Feature</span>
 
             </div>
             <div className='flex flex-wrap mt-10 lg:mt-20'>
-                {features.map ((items, index ) =>(
+                {features.map ((features, index ) =>(
                 <div key={index} className='w-full sm:1/2 lg:w-1/3'>
                     <div className='flex'>
                         <div className='flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700
@@ -23,6 +23,21 @@ px-2 py-1 uppercase '>Feature</span>
                 </div>
              ))}
             </div>
+
+            <div className='flex flex-wrap mt-10 lg:mt-20'>
+                {features.map ((features, index ) =>(
+                <div key={index} className='w-full sm:1/2 lg:w-1/3'>
+                    <div className='flex'>
+                        <div className='flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700
+                        justify-center items-center rounded-full'>
+                            {features.icon}
+                        </div>
+                    </div>
+                </div>
+             ))}
+            </div>
+
+           
 
         </div>
     )
