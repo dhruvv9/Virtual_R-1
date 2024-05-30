5f6b95e27742b69a73b94ef938fb8329dcb10723
dhruvv99 <dhruvakanive4468@gmail.com>
2024-05-30 23:26:56 +0530

    updated according to the product

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -12,17 +12,26 @@ px-2 py-1 uppercase '>Feature</span>
 
             </div>
             <div className='flex flex-wrap mt-10 lg:mt-20'>
-                {features.map ((items, index ) =>(
+                {features.map ((features, index ) =>(
                 <div key={index} className='w-full sm:1/2 lg:w-1/3'>
                     <div className='flex'>
                         <div className='flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700
                         justify-center items-center rounded-full'>
                             {features.icon}
                         </div>
+                        <div>
+                            <h3 className='mt-1 mb-6 text-xl'>{features.text}</h3>
+                            <p className='text-md p-2 mb-20 text-neutral-500'>
+                                {features.description}
+                            </p>
+                        </div>
                     </div>
                 </div>
              ))}
             </div>
+
+            
+           
 
         </div>
     )
